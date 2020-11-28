36aee77a77cdfe17f95c062273e84f2b259c0de9
Jen Speak <jspeak6@gmail.com>
2020-11-27 22:57:39 -0800

    added console logs with bold text for folders

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-require('./myscript.js');
+#!/usr/bin/env node
 
-console.log(require.cache);
+const fs = require('fs');
+const chalk = require('chalk');
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+    if (err) {
+        // error handling code here
+        console.log(err);
+    }
+
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        if (stats.isFile()) {
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.bold(filenames[index]));
+        }
+    }
+});
+
+// Method #1
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+
+//             resolve(stats);
+//         });
+//     });
+// };
